1c25a496274038c1b6a0fb2efd8e059e08fc279b
DionSot03 <dionsot03>
2025-06-06 15:21:39 +0000

    commit

---
+++ pages/CategoryView/jsobjects/JSObject4/JSObject4.js
@@ -0,0 +1,7 @@
+export default {
+	saveCartId: function() {
+		const id = ApiCreateCart.data.cart_id;
+		storeValue("cart_id", id);
+		showAlert("Αποθηκεύτηκε το καλάθι με id: " + id, "success");
+	}
+}
